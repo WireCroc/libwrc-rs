279d609e2509d4cffcd80459ca9664b3fed1ac84
nikolaycc <nikogogitadze2@gmail.com>
2023-01-27 00:59:12 +0400

    Added wrc structs and functions

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,15 @@ pub const MAX_PA: usize = 3;
 pub const MAX_IFACE: usize = 10;
 pub const MAX_IFNAME: usize = 16;
 
+#[derive(Debug)]
+#[repr(C)]
+pub struct wc_sockaddr {
+    pub sa_family: libc::sa_family_t,
+    pub sa_data: [libc::c_char; 14],
+}
+
 #[allow(non_camel_case_types)]
+#[derive(Debug)]
 #[repr(C)]
 pub enum pa {
     PA_NULL = 0,
@@ -19,6 +27,19 @@ pub enum pa {
 }
 
 #[allow(non_camel_case_types)]
+#[derive(Debug)]
+#[repr(C)]
+pub struct wc_pa {
+    eth: wc_eth,
+    arp: wc_arp,
+    ip: wc_ip,
+    tcp: wc_tcp,
+    udp: wc_udp,
+    p: [pa; MAX_PA]
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
 #[repr(C)]
 pub struct wc_eth {
     source: *mut libc::c_uchar,
@@ -27,6 +48,7 @@ pub struct wc_eth {
 }
 
 #[allow(non_camel_case_types)]
+#[derive(Debug)]
 #[repr(C)]
 pub struct wc_arp {
     hw_t: *mut libc::c_char,
@@ -41,6 +63,7 @@ pub struct wc_arp {
 }
 
 #[allow(non_camel_case_types)]
+#[derive(Debug)]
 #[repr(C)]
 pub struct wc_ip {
     version: libc::c_uchar,
@@ -53,6 +76,29 @@ pub struct wc_ip {
     source: *mut libc::c_uchar,
     dest: *mut libc::c_uchar,
     proto: libc::c_uchar
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+#[repr(C)]
+pub struct wc_tcp {
+    source: libc::c_ushort,
+    dest: libc::c_ushort,
+    sequence: libc::c_ulong,
+    ack_sequnce: libc::c_ulong,
+    window: libc::c_ushort,
+    checksum: libc::c_ushort,
+    flag: libc::c_uchar
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+#[repr(C)]
+pub struct wc_udp {
+    source: libc::c_ushort,
+    dest: libc::c_ushort,
+    check: libc::c_ushort,
+    len: libc::c_ushort
 }
 
 #[allow(non_camel_case_types)]
@@ -72,7 +118,28 @@ pub struct wc_iflist {
     len: libc::c_uchar
 }
 
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+#[repr(C)]
+pub struct wrc {
+    fd: libc::c_int,
+    recvl: libc::c_int,
+    recvn: libc::c_ulonglong,
+    iface: wc_iface,
+    ign_pa: pa,
+    flag: libc::c_char,
+    saddr: wc_sockaddr,
+    recv: *mut libc::c_uchar
+}
+
 extern "C" {
+    pub fn wrc_default(w: *mut wrc) -> libc::c_void;
+    pub fn wrc_destroy(w: *mut wrc) -> libc::c_void;
+
+    pub fn wrc_setopts(w: *mut wrc, ifc: wc_iface, p: pa, flag: libc::c_char) -> libc::c_char;
+    pub fn wrc_cap(w: *mut wrc, fp: libc::c_uchar, cb: extern "C" fn(wc_pa, *mut libc::FILE)) -> libc::c_char;
+    pub fn DEAFULT_CAP(wcp: wc_pa, fp: *mut libc::FILE) -> libc::c_void;
+    
     pub fn wrc_get_interfaces() -> wc_iflist;
 }
 
